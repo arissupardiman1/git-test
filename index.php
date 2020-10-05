1dd19e64713b189a3a76296a25f5839622e969fe
Aris Supardiman <supardimanaris@gmail.com>
2020-10-05 10:32:52 +0700

    modif index

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1> Hello World </h1>
+    <p> Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an </p>
 </body>
 </html>
